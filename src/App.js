7e4d12da80907886184ffd97f8499cb0f8dd752a
JH <imwisdomh@gmail.com>
2024-08-03 16:58:52 +0900

    react-router-dom 설치 및 페이지 라우팅 설정

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Friend from "./pages/Friend";
+import MyPage from "./pages/MyPage";
 
 const App = () => {
     return (
-        <div>
-            <h1>Hello</h1>
-        </div>
+        <Router>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/friend" element={<Friend />} />
+                <Route path="/my-page" element={<MyPage />} />
+            </Routes>
+        </Router>
     );
 };
 
