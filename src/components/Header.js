c1c4a9dbf0b9feeb944ca6ef330062326e49271d
JH <imwisdomh@gmail.com>
2024-08-06 00:41:15 +0900

    NavBar, Header 컴포넌트 스타일을 글로벌 CSS에서 모듈 CSS로 분리

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,5 @@
 import { useLocation } from "react-router-dom";
+import styles from "./Header.module.css";
 
 const Header = () => {
     const location = useLocation();
@@ -16,8 +17,8 @@ const Header = () => {
     };
 
     return (
-        <div className="header">
-            <h1 className="header__title">{getTitle(location.pathname)}</h1>
+        <div className={styles.header}>
+            <h1 className={styles.header__title}>{getTitle(location.pathname)}</h1>
         </div>
     );
 };
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,25 +1,26 @@
 import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faUser, faHouse, faUserGroup } from "@fortawesome/free-solid-svg-icons";
+import styles from "./NavBar.module.css";
 
 const NavBar = () => {
     return (
-        <nav className="nav">
-            <ul className="nav__list">
-                <li className="nav__btn">
-                    <Link className="nav__link" to="/">
+        <nav className={styles.nav}>
+            <ul className={styles.nav__list}>
+                <li className={styles.nav__btn}>
+                    <Link className={styles.nav__link} to="/">
                         <FontAwesomeIcon icon={faHouse} size="lg" />
                         Home
                     </Link>
                 </li>
-                <li className="nav__btn">
-                    <Link className="nav__link" to="/friend">
+                <li className={styles.nav__btn}>
+                    <Link className={styles.nav__link} to="/friend">
                         <FontAwesomeIcon icon={faUserGroup} size="lg" />
                         Friend
                     </Link>
                 </li>
-                <li className="nav__btn">
-                    <Link className="nav__link" to="/my-page">
+                <li className={styles.nav__btn}>
+                    <Link className={styles.nav__link} to="/my-page">
                         <FontAwesomeIcon icon={faUser} size="lg" />
                         My Page
                     </Link>
