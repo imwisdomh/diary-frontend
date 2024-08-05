05aac03fe1a547047feebe2a33aaa9273a372a6b
JH <imwisdomh@gmail.com>
2024-08-06 01:39:51 +0900

    Profile 컴포넌트와 스타일 생성

---
+++ src/components/Profile.js
@@ -0,0 +1,11 @@
+import styles from "./Profile.module.css";
+
+const Profile = () => {
+    return (
+        <div className={styles.profile}>
+            <h1>Profile</h1>
+        </div>
+    );
+};
+
+export default Profile;
