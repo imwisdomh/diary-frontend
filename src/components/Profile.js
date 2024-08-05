4696ef181e728b3bd86719f7d44bc917650b7f22
JH <imwisdomh@gmail.com>
2024-08-06 02:38:22 +0900

    Profile 컴포넌트에 props(img, username, bio) 추가

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,9 +1,13 @@
 import styles from "./Profile.module.css";
 
-const Profile = () => {
+const Profile = ({ img, username, bio }) => {
     return (
         <div className={styles.profile}>
-            <h1>Profile</h1>
+            <div className={styles.profile__img}>
+                <img src={img} alt="Profile" />
+            </div>
+            <h1 className={styles.profile__username}>{username}</h1>
+            <p className={styles.profile__bio}>{bio}</p>
         </div>
     );
 };
