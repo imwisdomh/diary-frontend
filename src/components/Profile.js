ba221fad0c0124a7d3b1248c403ebca5f5183e13
JH <imwisdomh@gmail.com>
2024-08-08 17:45:00 +0900

    Profile 컴포넌트 props 이름 변경(img -> avatar)

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,10 +1,10 @@
 import styles from "./Profile.module.css";
 
-const Profile = ({ img, username, bio }) => {
+const Profile = ({ avatar, username, bio }) => {
     return (
         <div className={styles.profile}>
             <div className={styles.profile__img}>
-                <img src={img} alt="Profile" />
+                <img src={avatar} alt="Profile" />
             </div>
             <h1 className={styles.profile__username}>{username}</h1>
             <p className={styles.profile__bio}>{bio}</p>
