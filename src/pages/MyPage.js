ea7abf20cc4bc9c72a652f5d2981fc2b9de665a6
JH <imwisdomh@gmail.com>
2024-08-09 21:26:53 +0900

    더미 데이터의 사용자 정보를 MyPage 컴포넌트에 표시

--- src/pages/MyPage.js
+++ src/pages/MyPage.js
@@ -1,10 +1,24 @@
+import { useState, useEffect } from "react";
 import Header from "../components/Header";
 import NavBar from "../components/NavBar";
+import Profile from "../components/Profile";
+import users from "../data/users.json";
 
 const MyPage = () => {
+    const [currentUser, setCurrentUser] = useState(null);
+
+    useEffect(() => {
+        setCurrentUser(users[0]);
+    }, []);
+
+    if (!currentUser) return null;
+
     return (
         <div className="container">
             <Header />
+            <div className="my-page">
+                <Profile username={currentUser.username} bio={currentUser.bio} avatar={currentUser.avatar} />
+            </div>
             <NavBar />
         </div>
     );
