8406409c9c8d4ebd20afecb4f12fba6c3fd38ac5
JH <imwisdomh@gmail.com>
2024-08-10 21:12:02 +0900

    Friend 컴포넌트 스타일 클래스 네이밍 개선 및 Profile 컴포넌트 props 이름 변경(img -> avatar) 적용

--- src/pages/Friend.js
+++ src/pages/Friend.js
@@ -10,65 +10,84 @@ const Friend = () => {
         <div className="container">
             <Header />
             <div className={styles.friend}>
-                <div className={styles.friend__search}>
-                    <FontAwesomeIcon icon={faMagnifyingGlass} size="lg" />
-                    <input type="text" placeholder="친구 검색" />
+                <div className={styles["friend__search"]}>
+                    <div className={styles["friend__search-input"]}>
+                        <FontAwesomeIcon icon={faMagnifyingGlass} size="lg" />
+                        <input type="text" placeholder="친구 검색" />
+                    </div>
+                    <div className={styles["friend__search-result"]}>
+                        <ul>
+                            <li>
+                                <Profile
+                                    username="Kim Cheolsu"
+                                    bio="아이 엠 그라운드 자기소개 하기"
+                                    avatar="https://avatars.githubusercontent.com/u/150656382?s=400&v=4"
+                                />
+                            </li>
+                            <li>
+                                <Profile
+                                    username="Kim Minji"
+                                    avatar="https://avatars.githubusercontent.com/u/150656382?s=400&v=4"
+                                />
+                            </li>
+                        </ul>
+                    </div>
                 </div>
-                <div className={styles.friend__request}>
+                <div className={styles["friend__requests"]}>
                     <span>친구 요청</span>
-                    <div className={styles.friend__status}>
-                        <div className={styles.friend__sent}>
+                    <div className={styles["friend__request-status"]}>
+                        <div className={styles["friend__request-sent"]}>
                             <span>보냄</span>
                             <ul>
                                 <li>
                                     <Profile
                                         username="Kim Cheolsu"
                                         bio="아이 엠 그라운드 자기소개 하기"
-                                        img="https://avatars.githubusercontent.com/u/150656382?s=400&v=4"
+                                        avatar="https://avatars.githubusercontent.com/u/150656382?s=400&v=4"
                                     />
                                 </li>
                                 <li>
                                     <Profile
                                         username="Kim Minji"
-                                        img="https://avatars.githubusercontent.com/u/150656382?s=400&v=4"
+                                        avatar="https://avatars.githubusercontent.com/u/150656382?s=400&v=4"
                                     />
                                 </li>
                             </ul>
                         </div>
-                        <div className={styles.friend__received}>
+                        <div className={styles["friend__request-received"]}>
                             <span>받음</span>
                             <ul>
                                 <li>
                                     <Profile
                                         username="Lee Cheolsu"
                                         bio="아이 엠 그라운드 자기소개 하기"
-                                        img="https://avatars.githubusercontent.com/u/150656382?s=400&v=4"
+                                        avatar="https://avatars.githubusercontent.com/u/150656382?s=400&v=4"
                                     />
                                 </li>
                                 <li>
                                     <Profile
                                         username="Lee Minji"
-                                        img="https://avatars.githubusercontent.com/u/150656382?s=400&v=4"
+                                        avatar="https://avatars.githubusercontent.com/u/150656382?s=400&v=4"
                                     />
                                 </li>
                             </ul>
                         </div>
                     </div>
                 </div>
-                <div className={styles.friend__list}>
+                <div className={styles["friend__friends-list"]}>
                     <span>친구 목록</span>
                     <ul>
                         <li>
                             <Profile
                                 username="Park Cheolsu"
                                 bio="아이 엠 그라운드 자기소개 하기"
-                                img="https://avatars.githubusercontent.com/u/150656382?s=400&v=4"
+                                avatar="https://avatars.githubusercontent.com/u/150656382?s=400&v=4"
                             />
                         </li>
                         <li>
                             <Profile
                                 username="Park Minji"
-                                img="https://avatars.githubusercontent.com/u/150656382?s=400&v=4"
+                                avatar="https://avatars.githubusercontent.com/u/150656382?s=400&v=4"
                             />
                         </li>
                     </ul>
